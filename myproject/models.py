11c730266eb67923a6e0ca1a566b21cded89b64a
arungothwal1077 <arungothwal1077@gmail.com>
2025-08-19 21:20:22 +0530

    33039

--- myproject/models.py
+++ myproject/models.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from djnago.urls import path
 from django.shortcuts import render
+from djnago.conf import settings
 
 
 #view function 
@@ -9,6 +10,7 @@ def home(request):
     context = {
         'restaurant_name' : 'Chtkara Bites'
         'message': 'welcome to our restaurant'
+        'phone': settings.PHONE_NUMBER
     }
     return render(request, 'home.html', context)
 
@@ -21,6 +23,8 @@ urlpatterns = [
 ]
 
 """
+#settins.py (add at bottom)
+PHONE_NUMBER = "+919068245991"
  creat a file : template/home.html
 <!DOCTYPE html>
 <html>
@@ -30,6 +34,7 @@ urlpatterns = [
 <body>
     <h1>{{ restaurant_name }}</h1>
     <p>{{ message }}</p>
+    <p>contact us: {{ phone }}</p> 
 </body>
 <html>
 """
